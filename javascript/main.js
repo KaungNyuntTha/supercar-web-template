fb5cee6df52f832ccecb0b578c04bbf4d6558fef
hydou-issei <riasgremory.dxd2022@gmail.com>
2023-08-26 00:29:01 +0630

    complete

--- javascript/main.js
+++ javascript/main.js
@@ -5,3 +5,42 @@ function toggleMenu() {
     menu.classList.toggle('active');
     nav.classList.toggle('active');
 }
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailer = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+        }
+    });
+    models.forEach(model => {
+        model.classList.remove('active');
+        if (model.classList.contains(name)) {
+            model.classList.add('active');
+        }
+    });
+}
+
+function togglePlay() {
+    const play = document.querySelector('.play');
+    const pause = document.querySelector('.pause');
+    play.classList.toggle('active');
+    pause.classList.toggle('active');
+}
+function pauseVideo() {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    bgVideoList.forEach(video => {
+        video.pause();
+    });
+    togglePlay();
+}
+function playVideo() {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    bgVideoList.forEach(video => {
+        video.play();
+    });
+    togglePlay();
+}
